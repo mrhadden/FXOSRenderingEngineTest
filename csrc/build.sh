7f3e14952d641a43676109e3d13840890baf8bc1
Mike Bush <mike.bush@usda.gov>
2024-01-05 15:19:15 -0600

    build and screenshot

--- csrc/build.sh
+++ csrc/build.sh
@@ -1,1 +1,5 @@
-x86_64-w64-mingw32-g++ -municode winmain.c Point.cpp GFXRECT.cpp GFXRECTP.c List.c FXWindow.c -lgdi32
+#!/bin/bash
+# Proper header for a Bash script.
+# This is run from WSL term to keep things honest
+# Need x86_64-w64-mingw32-g++ installed in Ubuntu 
+x86_64-w64-mingw32-g++ -municode winmain.c Point.cpp GFXRECT.cpp GFXRECTP.c List.c FXWindow.c pgm.c -lgdi32
