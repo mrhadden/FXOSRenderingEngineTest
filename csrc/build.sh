a39d109a87ad0ec1aae97348575adbfd721a15e6
Mike Bush <mike.bush@usda.gov>
2024-01-05 15:22:32 -0600

    Update build.sh

--- csrc/build.sh
+++ csrc/build.sh
@@ -3,3 +3,4 @@
 # This is run from WSL term to keep things honest
 # Need x86_64-w64-mingw32-g++ installed in Ubuntu 
 x86_64-w64-mingw32-g++ -municode winmain.c Point.cpp GFXRECT.cpp GFXRECTP.c List.c FXWindow.c pgm.c -lgdi32
+
