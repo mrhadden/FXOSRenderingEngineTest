01d29de7418600007ffeb7c12e10b0dd8ba4fe78
Mike Bush <mike.bush@usda.gov>
2024-01-12 18:46:38 -0600

    HAL

--- csrc/fxosw.c
+++ csrc/fxosw.c
@@ -5,7 +5,7 @@
 #include <windows.h>
 #include <windowsx.h>
 #include <stdio.h>
-#include <process.h>
+
 
 #include "FXWindow.h"
 #include "FXDevices.h"
@@ -91,7 +91,6 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			_hal_set_ctx(&ctx);
 			_hal_irq_signal(NULL,uMsg,wParam,lParam);
 			
-			ResumeThread(hCpuThread);
 		}
 		break;
     case WM_PAINT:
